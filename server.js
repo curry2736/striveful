47cbf4749855fad8fad81bbdb4f024423d64504d
curry2736 <arisrei.l.lim@gmail.com>
2020-06-20 00:00:16 -0700

    css and other things test

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.static(__dirname + '/views'));
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
---
+++ views/welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Password: <?php echo $_POST["password"]; ?><br>
+Confirmed: <?php echo $_POST["cpassword"]; ?><br>
+
+</body>
+</html>
