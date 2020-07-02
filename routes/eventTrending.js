ad2c48659f77eefe31a02e6281704e8c0401c494
Sid K <sidkhillon24@gmail.com>
2020-07-01 23:46:59 -0700

    eventTrending

--- routes/eventTrending.js
+++ routes/eventTrending.js
@@ -1,47 +1,18 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
-const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
-const { Internship, validate } = require('../models/internship');
-const { Volunteering, validate } = require('../models/volunteering');
-const { Workshop, validate } = require('../models/workshop');
+const { Internship} = require('../models/internship');
+const { Volunteering} = require('../models/volunteering');
+const { Workshop} = require('../models/workshop');
  
-router.post('/', async (req, res) => {
-    // First Validate The HTTP Request
-    console.log(req.body);
-    const { error } = validate(req.body);
-    console.log(error);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
+router.get('/', async (req, res) => {
+    let internships
+    try {
+        internships = await Internship.find().limit(5).exec()
+    } catch {
+        internships = []
     }
-
-    console.log(req.body)
- 
-    //  Now find the user by their email address
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        return res.status(400).send('Incorrect email or password.');
-    }
- 
-    // Then validate the Credentials in MongoDB match
-    // those provided in the request
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) {
-        return res.status(400).send('Incorrect email or password.');
-    }
-    const token = jwt.sign({ _id: user._id }, config.get('PrivateKey'));
-    res.send(token);
-});
- 
-function validate(req) {
-    const schema = {
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    };
- 
-    return Joi.validate(req, schema);
-}
+})
  
 module.exports = router; 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
+const { Internship} = require('../models/internship');
+const { Volunteering} = require('../models/volunteering');
+const { Workshop} = require('../models/workshop');
 
-router.get('/', (req, res) => {
-    res.render('index')
+ 
+router.get('/', async (req, res) => {
+    const internships = await Internship.find()
+    //const volunteerings = await Volunteering.find()
+    //const workshops = await workshops.find()
+
+    console.log(internships)
+    //console.log(volunteerings)
+    //console.log(workshops)
+
+    res.render('index', {internships: internships} )
 })
-
-module.exports = router;
+ 
+module.exports = router; 
