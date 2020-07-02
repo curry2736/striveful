bdf9b2453df797624b05c3fcfa31fe774c92c1bc
Sid K <sidkhillon24@gmail.com>
2020-07-01 22:06:20 -0700

    eventTrending

---
+++ routes/eventTrending.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const { Internship, validate } = require('../models/internship');
+const { Volunteering, validate } = require('../models/volunteering');
+const { Workshop, validate } = require('../models/workshop');
+ 
+router.post('/', async (req, res) => {
+    // First Validate The HTTP Request
+    console.log(req.body);
+    const { error } = validate(req.body);
+    console.log(error);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    console.log(req.body)
+ 
+    //  Now find the user by their email address
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).send('Incorrect email or password.');
+    }
+ 
+    // Then validate the Credentials in MongoDB match
+    // those provided in the request
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).send('Incorrect email or password.');
+    }
+    const token = jwt.sign({ _id: user._id }, config.get('PrivateKey'));
+    res.send(token);
+});
+ 
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+ 
+    return Joi.validate(req, schema);
+}
+ 
+module.exports = router; 
