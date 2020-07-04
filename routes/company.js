faa6bc6fc2b34e0ce5185cfa0e9396e61a6cd1d4
RoSriv30 <64049464+RoSriv30@users.noreply.github.com>
2020-07-04 16:58:13 -0700

    Update company.js

--- routes/company.js
+++ routes/company.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const { Internship, validate } = require('../models/internship');
+const { Internship } = require('../models/internship');
 
 
 router.get('/', (req, res) => {
-    res.render('company')
+    res.render('company', { internship: new Internship() })
     
 })
 
 router.post('/', async (req, res) => {
     // First Validate The Request
-    const { error } = validate(req.body);
-    console.log(validate(req.body));
-    console.log(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
+    // const { error } = validate(req.body);
+    // console.log(validate(req.body));
+    // console.log(req.body);
+    // if (error) {
+    //     return res.status(400).send(error.details[0].message);
+    // }
+    const internship = new Internship({
+        jobTitle: req.body.jobTitle,
+        description: req.body.description
+    })
+    internship.save((err, newInternship) => {
+        if (err) {
+            res.render('company', {
+                internship: internship,
+                errorMessage: 'Fool'
+            })
+        } else {
+            res.redirect('company')
+        }
+    })
+    //res.send(req.body.jobTitle)
 
 })
 
