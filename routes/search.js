23839b20f8b5522686dfa185cda6767d6f1e865a
Pranay Jha <pranay.jha@gmail.com>
2020-07-04 17:24:10 -0700

    fix search.js so that it actually works

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,25 @@
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
+const { Internship} = require('../models/internship');
+const { Volunteering} = require('../models/volunteering.js');
+const { Workshop} = require('../models/workshop');
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
+    res.render('search')
+})
+
+router.get('/query', async(req, res) => {
+
+    var title = req.params.jobTitle;
+
+    let internships = await Internship.find({"jobTitle": /title/})
+    console.log(internships)
+    
+    internships.forEach(internship => {
+        console.log('Hello')
+    })
     res.render('search')
 })
 
