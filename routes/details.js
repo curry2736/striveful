b3134b97d9f3c445075719c0dd92fe4b6f0aa058
Mohit Sridhar <mohitgsridhar@gmail.com>
2020-07-11 21:59:34 -0700

    finished details page

--- routes/details.js
+++ routes/details.js
@@ -8,24 +8,33 @@ const e = require('express');
 let ObjectID = require('mongodb').ObjectID;
 
 
-router.get('/', (req,res) => {
-    res.render('details');
-});
-
 //Show details
 router.get('/:id', async (req,res) =>{
     try {
         console.log(req.params.id); 
+
         var result = await Internship.findById(req.params.id);
         if (!result){
-            var result = await Volunteering.findById(req.params.id);
-            
-        } else if (!result){
-            var result = await Workshop.findById(req.params.id);
-        }; 
-        //console.log(result);
-        console.log(result)
-        res.render('details.ejs', {detail: {result: result}});
+            var result = await Volunteering.findById(req.params.id).exec();
+            if (!result){
+                var result = await Workshop.findById(req.params.id);
+            }
+        };
+        console.log(result);
+
+        if (!result.jobTitle){
+            var name = result.eventName;
+        } else if (!result.eventName){
+            var name = result.jobTitle;
+        };
+
+        if (!result.companyName){
+            var org = result.organization;
+        } else if (!result.organization){
+            var org = result.companyName;
+        };
+
+        res.render('details.ejs', {details: {result: result, name: name, org: org}});
     } catch (err){
         console.log(err);
         res.redirect('/');
