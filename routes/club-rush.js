f45f16adbcbd7d7ead9103fa3c5661ab5a829127
curry2736 <arisrei.l.lim@gmail.com>
2020-07-27 17:58:27 -0700

    club rush page fix

--- routes/club-rush.js
+++ routes/club-rush.js
@@ -44,12 +44,13 @@ router.get('/', async(req, res) => {
 
 router.get('/query', async (req, res) => {
     try {
+        
         const userVerification = isUser(req, res);
         let user = null
         if (userVerification != null ){
             user = await User.findOne({ _id: userVerification });
         }
-        console.log(user)
+        //console.log(user)
 
         var searchNum = null
         var totalResults = 0
@@ -72,24 +73,8 @@ router.get('/query', async (req, res) => {
         let volunteerings =  await Volunteering.find({"eventName": {$regex:name,$options:'i'}}).exec()
         let workshops = await Workshop.find({"eventName": {$regex:name,$options:'i'}}).exec()
         
-        if (internshipIsChecked) {
-            totalResults += internships.length
-            internships.forEach(internship => {
-                console.log(internship.jobTitle)
-            })
-        }
-        if (volunteeringIsChecked) {
-            totalResults += volunteerings.length
-            volunteerings.forEach(volunteering => {
-                console.log(volunteering.eventName)
-            })
-        }
-        if (workshopIsChecked) {
-            totalResults += workshops.length
-            workshops.forEach(workshop => {
-                console.log(workshop.eventName)
-            })
-        }
+        totalResults = volunteerings.length;
+
         console.log('total results: ' + totalResults)
 
         if (totalResults == 1) {
@@ -101,7 +86,7 @@ router.get('/query', async (req, res) => {
 
         console.log('--------------------------------------------------------------------')
         
-        res.render('search', {results : {user, searchNum, internships : internships, volunteerings : volunteerings, workshops : workshops, internshipIsChecked, volunteeringIsChecked, workshopIsChecked,
+        res.render('club-rush', {results : {user, searchNum, internships : internships, volunteerings : volunteerings, workshops : workshops, internshipIsChecked, volunteeringIsChecked, workshopIsChecked,
                                         opportunityPlaceholder, locationPlaceholder}})
     }
     catch (err) {
