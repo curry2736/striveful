faf0712182e06d8dd53cc151c3914c78c6a6476c
curry2736 <arisrei.l.lim@gmail.com>
2020-07-30 16:07:25 -0700

    centering stuff

--- routes/details.js
+++ routes/details.js
@@ -14,6 +14,7 @@ router.get('/:id', async (req,res) =>{
     try {
         //console.log(req.params.id); 
         const id =  isUser(req, res);
+        console.log(id)
         const user = await User.findById(id);
         let type = "";
         var result = await Internship.findById(req.params.id);
